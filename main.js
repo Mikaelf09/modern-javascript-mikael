b7f6d1c08136a9afddec18dedb8175967db628f2
Mikael Feldt <mikael@Mikael-sin-MacBook-Air.local>
2025-11-04 14:23:53 +0100

    slettet på oppgave1

--- main.js
+++ main.js
@@ -6,4 +6,4 @@ console.log("Hei,jeg heter MIkael!")
 
 console.log("string concatenation er flere variabler satt sammen av + tegnet")
 
-console.log("vis poenget er å vise alder og når geir er født, vil den printe først ut året han er født, også alderen hans."); hei
+console.log("vis poenget er å vise alder og når geir er født, vil den printe først ut året han er født, også alderen hans."); 
