f9ca76183c73801b8f275c8d7af5aafe12de6ae2
Mikael Feldt <mikael@Mikael-sin-MacBook-Air.local>
2025-11-04 14:17:58 +0100

    oppgave 1 ferdig

---
+++ mikael.js
@@ -0,0 +1,9 @@
+
+
+console.log("Hello World!")
+
+console.log("Hei,jeg heter MIkael!")
+
+console.log("string concatenation er flere variabler satt sammen av + tegnet")
+
+console.log("vis poenget er å vise alder og når geir er født, vil den printe først ut året han er født, også alderen hans."); hei
